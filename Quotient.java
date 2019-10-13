8914669842ac12cf7447d52c057de41a6cb167ce
Manikandaprabu1610 <54679448+Manikandaprabu1610@users.noreply.github.com>
2019-10-13 23:48:50 +0530

    quotient and remainder
    to find quotient and remainder of the prog without using(/,%) symbols

---
+++ Quotient.java
@@ -0,0 +1,24 @@
+
+public class Quotient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int numu=49;
+int deno=7;
+int quoti=0;
+while(numu>=deno){
+	
+
+	numu=numu-deno;
+quoti++;
+
+	}
+System.out.println("quotient is"+quoti);
+System.out.println("remainder is"+numu );
+	}
+	
+
+}
