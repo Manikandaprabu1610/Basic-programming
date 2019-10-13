a6a0e5b75a25050bdc841410076af3fc6e32ec89
Manikandaprabu1610 <54679448+Manikandaprabu1610@users.noreply.github.com>
2019-10-13 23:51:26 +0530

    split words and abcd pattern
    to split the words from the sentence and count the words
    A B C D E F G pattern program

---
+++ split words and pattern prog.java
@@ -0,0 +1,50 @@
+
+public class Words {
+
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*String s="hi how are you";
+
+String Data[]= s.split(" ");
+
+//for(int i=0;i<Data.length;i++)
+//{
+//System.out.println(Data[i]);	
+//
+
+System.out.println(Data.length);
+
+	}
+}
+
+		
+		/*char[] alpha = {'A','B','C','D','E','F','G'};
+		
+		for(int i=0;i<5;i++)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print(" "+alpha[j]);
+				
+			}
+			for(int j=i-1;j>=0;j--)
+		{
+				System.out.print(" " +alpha[j]);
+				
+			}
+			
+			System.out.println();
+		}
+	}
+	}*/
+
+int a[]={1,2,3,4,5};
+
+for(int i=a.length-1;i>=0;i--){
+	System.out.println(a[i]);
+}
+
+}
+}
